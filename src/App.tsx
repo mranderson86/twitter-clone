f41924a9635384821b59ca1830f7b806d7e33e6b
Anderson <mr.anderson.brito@gmail.com>
2020-07-07 19:55:37 -0300

    criado GlobalStyles component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React from "react";
 
-const App: React.FC = () => {
-    return (<div>Hello App</div>);
-}
+import GlobalStyles from "./styles/GlobalStyles";
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <h1>Hello App</h1>
+    </>
+  );
+};
 
 export default App;
