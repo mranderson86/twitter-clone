65bbb77d7dc37070609b7e4319346f959a1c8b56
Anderson <mr.anderson.brito@gmail.com>
2020-07-10 22:27:40 -0300

    Criado News componente

---
+++ src/components/News/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const News = () => (
+  <Container>
+    <span>Assuntos do momento no Brasil</span>
+    <strong>BootCamp da Rocketseat</strong>
+  </Container>
+);
+
+export default News;
--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import StickyBox from "react-sticky-box";
 
 import List from "../List";
 import FollowSuggestion from "../FollowSuggestion";
+import News from "../SideBar";
 
 import {
   Container,
@@ -18,21 +20,36 @@ const SideBar = () => (
       <SearchIcon />
     </SearchWrapper>
 
-    <Body>
-      <List
-        title="Talvez você curta"
-        elements={[
-          <FollowSuggestion name="Diego Hay" nickname="@diegohay23" />,
-          <FollowSuggestion name="Anderson Gomes" nickname="@mranderson286" />,
-          <FollowSuggestion name="Alexandre Brito" nickname="@alexandreb2" />,
-        ]}
-      />
+    <StickyBox>
+      <Body>
+        <List
+          title="Talvez você curta"
+          elements={[
+            <FollowSuggestion name="Diego Hay" nickname="@diegohay23" />,
+            <FollowSuggestion
+              name="Anderson Gomes"
+              nickname="@mranderson286"
+            />,
+            <FollowSuggestion name="Alexandre Brito" nickname="@alexandreb2" />,
+          ]}
+        />
 
-      <List
-        title="Talvez você curta"
-        elements={[<h1>TESTE</h1>, <h1>TESTE</h1>]}
-      />
-    </Body>
+        <List
+          title="Talvez você curta"
+          elements={[<News />, <News />, <News />]}
+        />
+
+        <List
+          title="Talvez você curta"
+          elements={[<News />, <News />, <News />]}
+        />
+
+        <List
+          title="Talvez você curta"
+          elements={[<News />, <News />, <News />]}
+        />
+      </Body>
+    </StickyBox>
   </Container>
 );
 
